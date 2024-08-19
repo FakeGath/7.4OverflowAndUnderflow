a7c803363d183160b5bd9c88f4740e2c7a910500
FakeGath <givanalvaradoath@gmail.com>
2024-08-18 19:15:03 -0600

    Solution for Overflow and underflow

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//Library that includes basic commands on c++
+#include <iostream>
+
+int main(){
+
+unsigned char data {250};
+
+++data;
+std::cout<<"data: "<<data<<std::endl;
+++data;
+std::cout<<"data: "<<data<<std::endl;
+++data;
+std::cout<<"data: "<<data<<std::endl;
+++data;
+std::cout<<"data: "<<data<<std::endl;
+++data;
+std::cout<<"data: "<<data<<std::endl;
+++data;
+std::cout<<"data: "<<data<<std::endl;
+++data;
+std::cout<<"data: "<<data<<std::endl;
+data = 0;
+std::cout<<"data: "<<data<<std::endl;
+data = 1;
+std::cout<<"data: "<<data<<std::endl;
+
+return 0;
+}
